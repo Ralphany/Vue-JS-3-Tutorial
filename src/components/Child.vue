240e67d1631ae539288a17718283108bc0b4924f
Raphael Anyidoho <raphaelanyidoho9@gmail.com>
2022-02-10 22:56:15 +0000

    Everything on Everything

---
+++ src/components/Child.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>Child Component</div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  beforeCreate() {
+    console.log("Child beforeCreate()");
+  },
+  created() {
+    console.log("Child created()");
+  },
+  beforeMount() {
+    console.log("Child beforeMount()");
+  },
+  mounted() {
+    console.log("Child mounted()");
+  },
+  beforeUpdate() {
+    console.log("Child beforeUpdate()");
+  },
+  updated() {
+    console.log("Child updated()");
+  },
+  beforeUnmount() {
+    console.log("Child beforeUnmount()");
+  },
+  Unmounted() {
+    console.log("Child Unmounted");
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    Parent Component
+    <button @click="showChild = !showChild">Toggle Child</button>
+    <Child v-if="showChild" />
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+export default {
+  name: "Parent",
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      showChild: true,
+    };
+  },
+  beforeCreate() {
+    console.log("Parent beforeCreate()");
+  },
+  created() {
+    console.log("Parent created()");
+  },
+  beforeMount() {
+    console.log("Parent beforeMount()");
+  },
+  mounted() {
+    console.log("Parent mounted()");
+  },
+  beforeUpdate() {
+    console.log("Parent beforeUpdate()");
+  },
+  updated() {
+    console.log("Parent updated()");
+  },
+  beforeUnmount() {
+    console.log("Parent beforeUnmount()");
+  },
+  Unmounted() {
+    console.log("Parent Unmounted");
+  },
+};
+</script>
+
+<style scoped>
+</style>
